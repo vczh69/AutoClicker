513fbeb7903f426aa1f6419da6448adf5232860f
Gor Mar <gormar69420@gmail.com>
2023-12-10 10:51:33 +0100

    Autoclicker setup

---
+++ AutocClicker.py
@@ -0,0 +1,32 @@
+import pyautogui
+import time
+import numpy as np
+from pynput.mouse import Controller, Button
+from pynput.keyboard import Listener, KeyCode
+import threading
+
+time.sleep(2)
+
+toggle_key = KeyCode(char='`')
+clicking = False
+mouse = Controller()
+
+def clicker(x, y):
+    while True:
+        if clicking:
+            mouse.click(Button.left, 1)
+            print("Turned on")
+        time.sleep(np.random.uniform(x, y))
+
+def toggle_event(key):
+    if key == toggle_key:
+        global clicking
+        clicking = not clicking
+        print("Turned off")
+
+click_thread = threading.Thread(target=clicker, args=(0.2, 2))
+click_thread.start()
+
+with Listener(on_press=toggle_event) as listener:
+    listener.join()
+
