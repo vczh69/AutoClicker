ffb03af031521c36a85c40538379f06f0775c3e0
Gor Mar <gormar69420@gmail.com>
2023-12-13 15:23:28 +0100

    Update

--- AutocClicker.py
+++ AutocClicker.py
@@ -4,12 +4,13 @@ from pynput.mouse import Controller, Button
 from pynput.keyboard import Listener, KeyCode
 import threading
 import tkinter as tk
+from tkinter import ttk
 
 class Autoclicker:
     def __init__(self, master):
         self.delay = (0.1, 0.1)
-        self.default_hotkey = KeyCode(char='`')
-        self.option_hotkey = ['`', 'F1', 'F2']
+        self.hotkey = KeyCode(char='`')
+        self.hotkey_options = ['`', 't', 'r']
         self.clicking = False
         self.mouse = Controller()
 
@@ -25,13 +26,32 @@ class Autoclicker:
         self.credits_label = tk.Label(master, text="By Gor Mar", font=("Arial", 10))
         self.credits_label.pack()
 
-        click_thread = threading.Thread(target=self.clicker, args=self.delay)
-        click_thread.start()
+        # Hotkey dropdown menu
+        self.select_label = tk.Label(master, text="Select hotkey:", font=("Arial", 15))
+        self.select_label.pack(pady=5)
+        
+        self.hotkey_var = tk.StringVar(master, value=self.hotkey.char)
+        self.hotkey_combobox = ttk.Combobox(master, textvariable=self.hotkey_var, values=self.hotkey_options, state="readonly", font=("Arial", 15))
+        self.hotkey_combobox.pack(pady=5)
 
+        # Create thread for GUI
+        self.gui_thread = threading.Thread(target=self.setup_gui)
+        self.gui_thread.daemon = True 
+        self.gui_thread.start()
+
+        # Create thread for autoclicker
+        self.click_thread = threading.Thread(target=self.clicker, args=(self.delay,))
+        self.click_thread.daemon = True
+        self.click_thread.start()
+
+        master.protocol("WM_DELETE_WINDOW", self.on_close)
+
+    def setup_gui(self):
         with Listener(on_press=self.toggle_event) as listener:
             listener.join()
 
-    def clicker(self, x, y):
+    def clicker(self, delay):
+        x, y = delay
         while True:
             if self.clicking:
                 self.mouse.click(Button.left, 1)
@@ -39,9 +59,17 @@ class Autoclicker:
             time.sleep(np.random.uniform(x, y))
 
     def toggle_event(self, key):
-        if self.default_hotkey == key:
+        pressed_key = str(key).replace("'", "")
+        selected_hotkey = str(self.hotkey_var.get())
+
+        if pressed_key == selected_hotkey:
             self.clicking = not self.clicking
             print("Turned off" if not self.clicking else "Turned on")
+
+    def on_close(self):
+        self.clicking = False
+        time.sleep(1)
+        self.master.destroy()
 
 if __name__ == "__main__":
     root = tk.Tk()
